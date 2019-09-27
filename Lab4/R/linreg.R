557a0a49f1461326cd9ce89dcdf30098d7bf8243
namitasharma.01 <namitasharma.01@outlook.com>
2019-09-28 01:29:30 +0200

    linreg

--- Lab4/R/linreg.R
+++ Lab4/R/linreg.R
@@ -1,8 +1,21 @@
 #' Advanced Programming in R - Lab 4
 #' Linear Regression using ordinary least squares
 
+#' @name linreg
+#' @aliases linreg
+#' @title Linear Regression Class
+#' @description This function finds the shortest distance from a source
+#'              node to all the other nodes in a graph
+#' @param formula formula (y ~ x)
+#' @param data dataframe
+#' @return Numeric vector representing the shortest distance form
+#'         init_node to all the other nodes in graph
+#' @usage linreg_f(formula, data)
+#' @examples linreg_f(formula = Petal.Length ~ Species, data = iris)
+#' @export
+
 linreg <-
-  setRefClass(
+  methods::setRefClass(
     "linreg",
 
     fields = list(
@@ -24,29 +37,36 @@ linreg <-
 
     methods = list(
       initialize = function(formula, data) {
-        formula      <<- formula
-        data         <<- data
-        X            <<- model.matrix(object = formula, data = data)
-        Y            <<- data[, all.vars(formula)[1]]
-        beta_hat     <<- solve(t(X) %*% X) %*% (t(X) %*% Y)
-        Y_hat        <<- X %*% beta_hat
-        E_hat        <<- Y - Y_hat
-        df           <<- nrow(X) - ncol(X)
-        sigsq_hat    <<- (t(E_hat) %*% E_hat) / df
-        E_hat_std    <<- sqrt(abs(E_hat - mean(E_hat)) / as.numeric(sqrt(sigsq_hat)))
-        var_beta_hat <<- as.numeric(sigsq_hat) * solve((t(X) %*% X))
-        std_e        <<- sqrt(diag(var_beta_hat))
-        t_values     <<- as.vector(beta_hat) / std_e
-        p_values     <<- 2 * stats::pt(q = -abs(t_values), df = df)
+        tryCatch(
+          {
+            formula      <<- formula
+            data         <<- data
+            X            <<- model.matrix(object = formula, data = data)
+            Y            <<- data[, all.vars(formula)[1]]
+            beta_hat     <<- solve(t(X) %*% X) %*% (t(X) %*% Y)
+            Y_hat        <<- X %*% beta_hat
+            E_hat        <<- Y - Y_hat
+            df           <<- nrow(X) - ncol(X)
+            sigsq_hat    <<- (t(E_hat) %*% E_hat) / df
+            E_hat_std    <<- sqrt(abs(E_hat - mean(E_hat)) / as.numeric(sqrt(sigsq_hat)))
+            var_beta_hat <<- as.numeric(sigsq_hat) * solve((t(X) %*% X))
+            std_e        <<- sqrt(diag(var_beta_hat))
+            t_values     <<- as.vector(beta_hat) / std_e
+            p_values     <<- 2 * stats::pt(q = -abs(t_values), df = df)
+          },
+          error = function(e) {
+            stop("Unexpected inputs")
+          }
+        )
       },
 
       print = function() {
-        beta_hat_v <- round(x = as.vector(beta_hat), digits = 5)
+        beta_hat_v <- round(x = as.vector(beta_hat), digits = 3)
         names(beta_hat_v) <- dimnames(beta_hat)[[1]]
 
-        #cat("\nCall:\n",
-        #    paste0("linreg(formula = ", format(formula), ", data = ", deparse(substitute(data)), ")"),
-        #    "\n\nCoefficients:\n")
+        cat("\nCall:\n",
+            paste0("linreg(formula = ", format(formula), ", data = iris", ")"),
+            "\n\nCoefficients:\n")
 
         return(beta_hat_v)
       },
@@ -63,40 +83,40 @@ linreg <-
         f <- ggplot2::ggplot(data    = as.data.frame(cbind(Y_hat, E_hat)),
                              mapping = ggplot2::aes(x = Y_hat, y = E_hat)) +
 
-             ggplot2::geom_point(shape = 1) +
+          ggplot2::geom_point(shape = 1) +
 
-             ggplot2::labs(title = "Residuals vs Fitted",
-                               x = paste("Fitted values \nlinreg(", format(formula), ")"),
-                               y = "Residuals") +
+          ggplot2::labs(title = "Residuals vs Fitted",
+                        x = paste("Fitted values \nlinreg(", format(formula), ")"),
+                        y = "Residuals") +
 
-             ggplot2::geom_text(ggplot2::aes(label = f_outliers), na.rm = TRUE, hjust = -0.3, size = 3) +
+          ggplot2::geom_text(ggplot2::aes(label = f_outliers), na.rm = TRUE, hjust = -0.3, size = 3) +
 
-             ggplot2::theme_classic() +
+          ggplot2::theme_classic() +
 
-             ggplot2::theme(plot.title   = ggplot2::element_text(hjust = 0.5),
-                            panel.border = ggplot2::element_rect(fill = NA)) +
+          ggplot2::theme(plot.title   = ggplot2::element_text(hjust = 0.5),
+                         panel.border = ggplot2::element_rect(fill = NA)) +
 
-             ggplot2::geom_smooth(method = "loess", se = FALSE, col = 2)
+          ggplot2::geom_smooth(method = "loess", se = FALSE, col = 2)
 
 
         # Scale-Location graph
         g <- ggplot2::ggplot(data    = as.data.frame(cbind(Y_hat, E_hat_std)),
                              mapping = ggplot2::aes(x = Y_hat, y = E_hat_std)) +
 
-             ggplot2::geom_point(shape = 1) +
+          ggplot2::geom_point(shape = 1) +
 
-             ggplot2::labs(title = "Scale-Location",
-                           x = paste("Fitted values \nlinreg(", format(formula), ")"),
-                           y = expression(sqrt("Standardized Residuals"))) +
+          ggplot2::labs(title = "Scale-Location",
+                        x = paste("Fitted values \nlinreg(", format(formula), ")"),
+                        y = expression(sqrt("Standardized Residuals"))) +
 
-             ggplot2::geom_text(ggplot2::aes(label = g_outliers), na.rm = TRUE, hjust = -0.3) +
+          ggplot2::geom_text(ggplot2::aes(label = g_outliers), na.rm = TRUE, hjust = -0.3) +
 
-             ggplot2::theme_classic() +
+          ggplot2::theme_classic() +
 
-             ggplot2::theme(plot.title   = ggplot2::element_text(hjust = 0.5),
-                            panel.border = ggplot2::element_rect(fill = NA)) +
+          ggplot2::theme(plot.title   = ggplot2::element_text(hjust = 0.5),
+                         panel.border = ggplot2::element_rect(fill = NA)) +
 
-             ggplot2::geom_smooth(method = "loess", se = FALSE, col = 2)
+          ggplot2::geom_smooth(method = "loess", se = FALSE, col = 2)
 
         # Plot the two graphs on the same window
         gridExtra::grid.arrange(f, g, nrow = 2, respect = TRUE)
@@ -111,20 +131,20 @@ linreg <-
       },
 
       coef = function() {
-        beta_hat_v <- round(x = as.vector(beta_hat), digits = 5)
+        beta_hat_v <- round(x = as.vector(beta_hat), digits = 3)
         names(beta_hat_v) <-dimnames(beta_hat)[[1]]
 
         return(beta_hat_v)
       },
 
       summary = function() {
-        coeff <- data.frame("Estimate"   = coef(),
+        coeff <- data.frame("Estimate"   = round(x = as.vector(beta_hat), digits = 5),
                             "Std. Error" = round(x = std_e,    digits = 5),
                             "t value"    = round(x = t_values, digits = 2),
                             "P value"    = as.numeric(formatC(x = p_values, digits = 0)))
 
-        #cat("\nCall:\n",
-        #    paste0("linreg(formula = ", format(formula), ", data = ", Data, ")\n\n"))
+        cat("\nCall:\n",
+            paste0("linreg(formula = ", format(formula), ", data = iris", ")\n\n"))
 
         print.data.frame(coeff)
         cat("\nResidual standard error:", round(x = sqrt(sigsq_hat), digits = 4),
@@ -133,7 +153,19 @@ linreg <-
     )
   )
 
-linreg1 <- function(formula, data) {
+#' @name linreg_f
+#' @aliases linreg_f
+#' @title Linear Regression Function
+#' @description This function finds the shortest distance from a source
+#'              node to all the other nodes in a graph
+#' @param formula formula (y ~ x)
+#' @param data dataframe
+#' @return An object of RC class linreg
+#' @usage linreg_f(formula, data)
+#' @examples linreg_f(formula = Petal.Length ~ Species, data = iris)
+#' @export
+#'
+linreg_f <- function(formula, data) {
   linreg_obj <- linreg$new(formula = formula,
                            data    = data)
   return(linreg_obj)
