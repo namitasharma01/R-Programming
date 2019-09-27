64b203d98a7fd2076ea81faf7558e3e9f9c60cd7
namitasharma.01 <namitasharma.01@outlook.com>
2019-09-27 23:42:53 +0200

    Travis badge

--- Lab3/R/Dijkstra.R
+++ Lab3/R/Dijkstra.R
@@ -4,7 +4,7 @@
 #' @name dijkstra
 #' @aliases dijkstra
 #' @rdname dijkstra
-#' @title Dijkstra Algorithm modified
+#' @title Dijkstra Algorithm
 #' @description This function finds the shortest distance from a source 
 #'              node to all the other nodes in a graph 
 #' @param graph Dataframe
